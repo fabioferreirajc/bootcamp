f2e4882a1101c7a57b35b7ef91fb7b7a63e593e5
Fábio Ferreira <fabioferreira.jc@gmail.com>
2018-07-13 18:11:22 +0100

    jdbc

---
+++ prompt-jdbc-skeleton/src/org/academiadecodigo/bootcamp/persistance/ConnectionManager.java
@@ -0,0 +1,40 @@
+package org.academiadecodigo.bootcamp.persistance;
+
+;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private Connection connection = null;
+    private String dbUrl= "jdbc:mysql://localhost:3306/ac";
+    private String user="root";
+    private String pass="";
+
+
+
+    public Connection getConnection() {
+
+        try {
+            if (connection == null) {
+
+                connection = DriverManager.getConnection(dbUrl, user, pass);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Failure to connect to database : " + ex.getMessage());
+        }
+        return connection;
+    }
+
+    public void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Failure to close database connections: " + ex.getMessage());
+        }
+    }
+}
