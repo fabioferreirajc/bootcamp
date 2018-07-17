af4495e8e1b3823a5bac3222315972e04ac162c6
Fábio Ferreira <fabioferreira.jc@gmail.com>
2018-07-17 16:37:08 +0100

    persitence hibernate

---
+++ hibernate/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
+
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("Result: " +
+                em.createNativeQuery("select 1 + 1").getSingleResult());
+
+        em.close();
+
+        emf.close();
+    }
+}
